59bfc3f4ac9a5499c17ac1a5bde5332c867b461c
JeroenDM <jeroendemaeyer@live.be>
2020-06-19 16:41:12 +0200

    add finite diff jacobian calculation to constriant tests

--- elion_planner/test/test_constraints.cpp
+++ elion_planner/test/test_constraints.cpp
@@ -7,6 +7,7 @@
 
 #include <moveit/robot_model_loader/robot_model_loader.h>
 #include <moveit/robot_model/robot_model.h>
+#include <moveit/robot_state/robot_state.h>
 #include <moveit_msgs/Constraints.h>
 
 // Names to read values from the parameter server
@@ -50,6 +51,24 @@ moveit_msgs::OrientationConstraint createOrientationConstraint(std::string& base
   return orientation_constraint;
 }
 
+Eigen::Vector3d rotationToRPY(const Eigen::Matrix3d& r)
+{
+  Eigen::Vector3d xyz = r.eulerAngles(0, 1, 2);
+  // in MoveIt some wrapping is done sometimes, but I'm not sure why or when to apply it yet
+  // probably to convert the eigen convention: [0:pi]x[-pi:pi]x[-pi:pi] to a MoveIt convention?
+  // but it looks the same to me
+  // it does not seem to work in some cases.
+  // xyz(0) = std::min(fabs(xyz(0)), M_PI - fabs(xyz(0)));
+  // xyz(1) = std::min(fabs(xyz(1)), M_PI - fabs(xyz(1)));
+  // xyz(2) = std::min(fabs(xyz(2)), M_PI - fabs(xyz(2)));
+  return xyz;
+}
+
+Eigen::Vector3d poseToRPY(const Eigen::Isometry3d& p)
+{
+  return rotationToRPY(p.rotation());
+}
+
 class TestConstraints : public ::testing::Test
 {
 protected:
@@ -61,7 +80,9 @@ protected:
     {
       robot_model_loader_ = std::make_shared<robot_model_loader::RobotModelLoader>(ROBOT_DESCRIPTION);
       robot_model_ = robot_model_loader_->getModel();
-      num_dofs_ = robot_model_->getJointModelGroup(group_name_)->getVariableCount();
+      robot_state_ = std::make_shared<robot_state::RobotState>(robot_model_);
+      joint_model_group_ = robot_state_->getJointModelGroup(group_name_);
+      num_dofs_ = joint_model_group_->getVariableCount();
     }
     else
     {
@@ -72,8 +93,35 @@ protected:
   {
   }
 
+  const Eigen::Isometry3d fk(const Eigen::VectorXd q) const
+  {
+    robot_state_->setJointGroupPositions(joint_model_group_, q);
+    return robot_state_->getGlobalLinkTransform(ee_link_name_);
+  }
+
+  Eigen::MatrixXd numericalJacobianRPY(const Eigen::VectorXd q)
+  {
+    const double h{ 1e-6 }; /* step size for numerical derivation */
+
+    Eigen::MatrixXd J = Eigen::MatrixXd::Zero(3, num_dofs_);
+
+    // helper matrix for differentiation.
+    Eigen::MatrixXd Ih = h * Eigen::MatrixXd::Identity(num_dofs_, num_dofs_);
+
+    for (std::size_t dim{ 0 }; dim < num_dofs_; ++dim)
+    {
+      auto rpy = poseToRPY(fk(q));
+      auto rpy_plus_h = poseToRPY(fk(q + Ih.col(dim)));
+      Eigen::Vector3d col = (rpy_plus_h - rpy) / h;
+      J.col(dim) = col;
+    }
+    return J;
+  }
+
   robot_model_loader::RobotModelLoaderPtr robot_model_loader_;
   robot_model::RobotModelPtr robot_model_;
+  robot_state::RobotStatePtr robot_state_;
+  const robot_state::JointModelGroup* joint_model_group_;
   std::string group_name_;
   std::string base_link_name_;
   std::string ee_link_name_;
@@ -108,6 +156,27 @@ TEST_F(TestConstraints, InitRPYConstraint)
   constraint_ = std::make_shared<elion::RPYConstraints>(robot_model_, group_name_, num_dofs_);
   constraint_->init(constraint_msgs);
 }
+
+/** TODO fix issue in this test **/
+// TEST_F(TestConstraints, RPYConstraintJacobian)
+// {
+//   moveit_msgs::Constraints constraint_msgs;
+//   constraint_msgs.orientation_constraints.push_back(createOrientationConstraint(base_link_name_, ee_link_name_));
+
+//   constraint_ = std::make_shared<elion::RPYConstraints>(robot_model_, group_name_, num_dofs_);
+//   constraint_->init(constraint_msgs);
+
+//   double total_error {999.9};
+//   double error_tolerance {1e-4};
+
+//   Eigen::VectorXd q = Eigen::VectorXd::Random(num_dofs_);
+//   auto J_exact = constraint_->calcErrorJacobian(q);
+//   auto J_finite_diff = numericalJacobianRPY(q);
+
+//   total_error = (J_exact - J_finite_diff).lpNorm<1>();
+//   EXPECT_LT(total_error, error_tolerance);
+// }
+
 
 int main(int argc, char** argv)
 {
