dac2d8a0c16fc23f99c2e5f46f17b0a6ac1ab356
JeroenDM <jeroendemaeyer@live.be>
2020-07-05 17:54:06 +0200

    add test for angular velocity conversion to rpy

--- elion_planner/test/test_conversions.cpp
+++ elion_planner/test/test_conversions.cpp
@@ -5,6 +5,40 @@
 
 using namespace Eigen;
 
+const int NUM_RANDOM_TESTS{ 10 };
+
+TEST(AngularVelocityToRPYRates, numericalDerivative)
+{
+  const double h{ 1e-8 };  // interval with for forward finite difference
+
+  for (int i{ 0 }; i < NUM_RANDOM_TESTS; ++i)
+  {
+    // create random orientation and angular velocity
+    Matrix3d rotation{ Quaterniond::UnitRandom() };
+    auto omega{ Vector3d::Random().normalized() };
+
+    // use fixed values to make test repeatable
+    // Matrix3d rotation{ AngleAxisd(0.3, Vector3d::UnitX()) * AngleAxisd(0.2, Vector3d::UnitY()) *
+    //                    AngleAxisd(0.1, Vector3d::UnitZ()) };
+    // Vector3d omega;
+    // omega << 0.3, 0.0, 0.0;
+
+    // apply angular velocity to the orientation for a time interval h
+    Matrix3d rotation_plus_h = AngleAxisd(h, omega) * rotation;
+
+    // calculate euler rates rates using forward difference approximation
+    Vector3d rpy = rotation.eulerAngles(0, 1, 2);
+    Vector3d rpy_dot_approx = (rotation_plus_h.eulerAngles(0, 1, 2) - rpy) / h;
+
+    Vector3d rpy_dot_exact = elion::angularVelocityToRPYRates(rpy[0], rpy[1]) * omega;
+
+    const double ERROR_TOLERANCE{ 1e-5 };
+    EXPECT_LT(std::abs(rpy_dot_exact[0] - rpy_dot_approx[0]), ERROR_TOLERANCE);
+    EXPECT_LT(std::abs(rpy_dot_exact[1] - rpy_dot_approx[1]), ERROR_TOLERANCE);
+    EXPECT_LT(std::abs(rpy_dot_exact[2] - rpy_dot_approx[2]), ERROR_TOLERANCE);
+  }
+}
+
 /** Conversion of the angular velocity vector omega
  * to roll pitch yaw angles.
  * (intrinsic XYZ rotation angles.)
