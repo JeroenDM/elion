3b067f93b15f379e6492d66688f1029570eb20c7
JeroenDM <jeroendemaeyer@live.be>
2020-07-21 10:59:23 +0200

    initialize robot state before solving the inverse kinematics

--- elion_examples/src/run_example.cpp
+++ elion_examples/src/run_example.cpp
@@ -63,12 +63,14 @@ planning_interface::MotionPlanRequest createPTPProblem(geometry_msgs::Pose& star
 
   // fill out start state in request
   robot_state::RobotState start_state(robot_model);
+  start_state.setToDefaultValues();
   bool success = start_state.setFromIK(joint_model_group, start_pose, 10.0);
   ROS_INFO_STREAM("Start pose IK: " << (success ? "succeeded." : "failed."));
   moveit::core::robotStateToRobotStateMsg(start_state, req.start_state);
 
   // fill out goal state in request
   robot_state::RobotState goal_state(start_state);
+  goal_state.setToDefaultValues();
   success = goal_state.setFromIK(joint_model_group, goal_pose, 10.0);
   ROS_INFO_STREAM("Goal pose IK: " << (success ? "succeeded." : "failed."));
   moveit_msgs::Constraints joint_goal = kinematic_constraints::constructGoalConstraints(goal_state, joint_model_group);
